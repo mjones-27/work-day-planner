0a4c449c9e5f97c7abe1ae06f9d89ebc7a5b1bfd
unknown <mars.jones34@gmail.com>
2020-03-19 22:56:39 -0700

    timeblocks on page.  color hardcoded.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,31 @@
+// set variables
 //get time
-const currTime = moment().format('MMMM Do YYYY');
+const currDay = moment().format('MMMM Do YYYY');
 
-console.log(currTime);
+console.log(currDay);
+
+var $timeBlocks = $(".time-block");
+
+// draw variables on page
 
 // update current time and date
 let $dateHeading = $('#currentDay');
-$dateHeading.text(currTime);
+$dateHeading.text(currDay);
+
+//update colors in timeblocks based on current time
+function timeblockInit(){
+    $timeBlocks.each(function(){
+        var $thisBlock = $(this);
+        var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
+
+        $thisBlock.addClass("past");
+
+
+    })
+}
+
+// draw empty timeblocks (to erase non-saved data)
+
+// fill saved timeblocks with saved data
+
+timeblockInit();
